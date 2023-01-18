ce1d9acb3610111be8d48ecc10b5be942b8854e9
Álvaro <user2725@salesianos-lacuesta.net>
2023-01-18 01:05:47 +0000

    experimento tres

--- src/App.java
+++ src/App.java
@@ -6,8 +6,10 @@ public class App {
     public static void main(String[] args) {
         Market mercado = new Market(5);
 
-        Farmer adrian = new Farmer(mercado, "Adrián", 5, 2000);
-        Client goyo = new Client(mercado, "Goyo", 5, 8000);
+        Farmer adrian = new Farmer(mercado, "Adrián", 5, 4000);
+        Client goyo = new Client(mercado, "Goyo", 5, 4000);
+        
+        adrian.setPriority(Thread.MAX_PRIORITY);
         
         adrian.start();
         goyo.start();
